23c3a349b34aa3d39ef067ae3e81fbf01a2c6eb8
fevzierenn <fevzierencicek@gmail.com>
2024-12-01 03:06:40 +0300

    cross origin eklendi

--- src/main/java/org/example/hansocial/responses/PostResponse.java
+++ src/main/java/org/example/hansocial/responses/PostResponse.java
@@ -3,6 +3,7 @@ package org.example.hansocial.responses;
 import org.example.hansocial.entities.Post;
 import lombok.Data;
 
+import java.util.Date;
 import java.util.List;
 
 @Data
@@ -14,6 +15,7 @@ public class PostResponse {
 	String title;
 	String text;
 	List<LikeResponse> postLikes;
+	Date createDate;
 	
 	public PostResponse(Post entity, List<LikeResponse> likes) {
 		this.id = entity.getId();
@@ -22,5 +24,6 @@ public class PostResponse {
 		this.title = entity.getTitle();
 		this.text = entity.getText();
 		this.postLikes = likes;
+		this.createDate = entity.getCreateDate();
 	}
 }
