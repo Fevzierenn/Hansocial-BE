e9e23d00bd7ce2ae74bf860b0a444ee82ebd029f
fevzierenn <fevzierencicek@gmail.com>
2026-07-07 00:49:54 +0300

    Fix Comment and CommentRequest

--- src/main/java/org/example/hansocial/entities/Comment.java
+++ src/main/java/org/example/hansocial/entities/Comment.java
@@ -2,7 +2,10 @@ package org.example.hansocial.entities;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 import org.hibernate.annotations.OnDelete;
 import org.hibernate.annotations.OnDeleteAction;
 
@@ -11,6 +14,9 @@ import java.util.Date;
 @Entity
 @Table(name="comment")
 @Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class Comment {
 
 	@Id
